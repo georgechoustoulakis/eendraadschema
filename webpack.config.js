2ab271f275b750041257b4da9ad95d6bd4efa35b
George Choustoulakis <george@choustoulakis.com>
2024-02-14 23:07:59 +0100

    Create npm package with simple webpack build

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const fs = require("fs");
+const webpack = require('webpack');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'eendraadschema.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new webpack.BannerPlugin({banner: fs.readFileSync('./LICENSE.md', 'utf8')}),
+    ],
+    optimization: {
+        minimizer: [new TerserPlugin({
+            extractComments: false,
+        })],
+    },
+};
