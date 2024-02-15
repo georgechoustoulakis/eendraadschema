34d454adc31003d64e98f5f38a1f93c58e5ee546
George Choustoulakis <george@choustoulakis.com>
2024-02-15 19:57:20 +0100

    Fix importing

--- src/main.ts
+++ src/main.ts
@@ -704,7 +704,7 @@ export function load_example(nr: number) {
     }
 }
 
-var importjson = function(event) {
+export function importJson(event) {
     var input = event.target;
     var reader = new FileReader();
     var text:string = "";
